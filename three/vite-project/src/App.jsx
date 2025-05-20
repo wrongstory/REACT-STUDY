20bf744e77889ed247ac36c79cd9b8696a494065
L <lmwoo0327@naver.com>
2025-05-20 11:21:11 +0900

    [React] 강의 코드 따라하기

---
+++ three/vite-project/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+import { Component, useState } from 'react';
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   return (
+//     <>
+//       <div>counter : {counter}</div>
+//       <button
+//         onClick={() => {
+//           setCounter((prev) => prev + 1);
+//         }}
+//       >+</button>
+//       <button
+//         onClick={() => {
+//           setCounter((prev) => prev - 1);
+//         }}
+//       >-</button>
+//     </>
+//   );
+// }
+
+class App extends Component {
+  state = { counter: 2 };
+  render() {
+    return (
+      <>
+        <div>counter {this.state.counter}</div>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter + 1 })}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState({ counter: this.state.counter - 1 })}
+        >
+          -
+        </button>
+      </>
+    );
+  }
+}
+
+export default App;
