fd32895fe9d4e3763fd1bdaa0e04edb5399ee0b7
L <lmwoo0327@naver.com>
2025-05-22 14:24:16 +0900

    [React] 5-3 생명주기 함수

--- five/src/App.jsx
+++ five/src/App.jsx
@@ -1,100 +1,64 @@
-import { Component, use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+
 function App() {
-  const [showCounter, setShowCounter] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data')
+      .then((res) => res.json())
+      .then((res) => setData(res));
+  }, []);
+  // 빈 배열 없으면 무한 리렌더링 일어남. 조심할 것!!!!
 
   return (
     <>
-      {showCounter && <Counter />}
-      <button onClick={() => setShowCounter((prev) => !prev)}>show?</button>
+      {/* <Component /> */}
+      {/* <Component2 /> */}
+      <div>데이터 목록</div>
+      {data.map((el) => (
+        <div key={el.id}>{el.content}</div>
+      ))}
     </>
   );
 }
 
-function Counter() {
-  const [counter, setCounter] = useState(1);
-  const [counter2, setCounter2] = useState(100);
+// 1. 서버에서 데이터를 받아올 때
+// function Component() {
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    // 컴포넌트 들어갈 경우 작동
-    console.log('useEffect');
+//   useEffect(() => {
+//     fetch('...').then((res) => setData(res));
+//   }, []);
 
-    // 컴포넌트 사라질때 작동
-    return () => {
-      console.log('return function');
-    };
-  }, []);
-  // 빈 배열을 추가하면 첫 화면 렌더 때만 실행됨
-  // 배열안에 특정 변수를 넣으면, 변수가 변경될 때 useEffect가 실행
-  // 함수 컴포넌트는 리렌더링이 발생할 경우에도 컴포넌트가 사라짐.
-  // 즉 정확히 타게팅을 하고 코드를 작성할 것
-
-  // 1. 컴포넌트가 최초로 렌더 될때에만 조작을 하고 싶다!
-  useEffect(() => {
-    console.log('맨 처음 렌더링 될 때');
-  }, []);
-
-  // 2. 컴포넌트가 리렌더링 될 때 조작하고 싶다!
-  useEffect(() => {
-    console.log('리렌더링...');
-  });
-
-  // 3. 특정 상태값이 변할 때에만 조작하고 싶다!
-  useEffect(() => {
-    console.log('counter의 값이 변할 때');
-  }, [counter]);
-
-  useEffect(() => {
-    console.log('counter2의 값이 변할 때');
-  }, [counter2]);
-
-  // 4. 컴포넌트가 최종적으로 언마운트 될때 조작하고 싶다!
-  useEffect(() => {
-    return () => {
-      console.log('retuned function');
-    };
-  }, []);
-
-  return (
-    <>
-      <div>counter : {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>+1</button>
-      <div>counter2 : {counter2}</div>
-      <button onClick={() => setCounter2(counter2 - 1)}>-1</button>
-    </>
-  );
-}
-
-// class Counter extends Component {
-//   constructor() {
-//     super();
-//     this.state = { counter: 1 };
-//     console.log('constructor');
-//   }
-
-//   componentDidMount() {
-//     console.log('componentDidMount');
-//   }
-
-//   componentDidUpdate() {
-//     console.log('componentDidUpdate');
-//   }
-//   componentWillUnmount() {
-//     console.log('componentWillUnmount');
-//   }
-//   render() {
-//     console.log('render');
-//     return (
-//       <>
-//         <div>counter : {this.state.counter}</div>
-//         <button
-//           onClick={() => this.setState({ counter: this.state.counter + 1 })}
-//         >
-//           +1
-//         </button>
-//       </>
-//     );
-//   }
+//   return (
+//     <>
+//       <div>데이터 목록</div>
+//       {data.map((el) => (
+//         <div>{el.content}</div>
+//       ))}
+//     </>
+//   );
 // }
+
+// 2. 이벤트 핸들러를 사용할 때
+// function Component2() {
+//   const [position, setPosition] = useState({ x: 0, y: 0 });
+
+//   useEffect(() => {
+//     const mouseHandler = () => {
+//       '마우스 위치 구해서 상태 업데이트 하는 코드';
+//     };
+
+//     window.addEventListener('mousemove', mouseHandler);
+//     return () => {
+//       window.removeEventListener('mousemove', mouseHandler);
+//     };
+//   });
+
+//   return <Component2>마우스 위치가 필요한 컴포넌트</Component2>;
+// }
+
+// 3. 타이머 함수를 사용할 때
 
 export default App;
