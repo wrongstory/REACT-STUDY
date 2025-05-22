1be45255987f0f5f42560de95672e31e63f18e98
L <lmwoo0327@naver.com>
2025-05-22 16:18:34 +0900

    [React] 5-4 타이머와 마우스follow, scroll

--- five/src/App.jsx
+++ five/src/App.jsx
@@ -19,6 +19,10 @@ function App() {
       {data.map((el) => (
         <div key={el.id}>{el.content}</div>
       ))}
+      <MouseFollower />
+      <ScrollIndicator />
+      <AlertTimer />
+      <div style={{ height: '300vh' }}></div>
     </>
   );
 }
@@ -59,6 +63,84 @@ function App() {
 //   return <Component2>마우스 위치가 필요한 컴포넌트</Component2>;
 // }
 
+// 마우스를 따라다니는 함수
+const MouseFollower = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+      console.log('mouseMoved!!');
+    };
+    window.addEventListener('mouseover', handleMouseMove);
+    return () => {
+      window.removeEventListener('mouseover', handleMouseMove);
+    };
+  });
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: position.y,
+        left: position.x,
+        width: '20px',
+        height: '20px',
+        borderRadius: '50%',
+        background: 'white',
+        transform: 'translate(-50%, -50%)',
+        pointerEvents: 'none', // 클릭을 방해하지 않음
+      }}
+    ></div>
+  );
+};
+
+const ScrollIndicator = () => {
+  const [scrollWidth, setScrollWidth] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const windowHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scrollPercentage = (scrollTop / windowHeight) * 100;
+      setScrollWidth(scrollPercentage);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: `${scrollWidth}%`,
+        height: '10px',
+        background: 'blue',
+      }}
+    ></div>
+  );
+};
+
 // 3. 타이머 함수를 사용할 때
 
+const AlertTimer = () => {
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    const setTimeoutId = setTimeout(() => {
+      showAlert === true ? alert('시간 초과') : null;
+    }, 3000);
+    return () => {
+      clearTimeout(setTimeoutId);
+    };
+  });
+
+  return <button onClick={() => setShowAlert(false)}>빨리 클릭!!!</button>;
+};
 export default App;
