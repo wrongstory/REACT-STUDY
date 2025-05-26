eda07c2fbebbd24d517d094735dd6a251d5ded0b
L <lmwoo0327@naver.com>
2025-05-26 10:53:21 +0900

    [React] Todo List 기본 레이아웃 세팅

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// 프로젝트 최소 요구 사항
+// 1. Todo 생성 / 조회 / 수정 / 삭제 (CRUD) 기능 구현
+// 2. 현재 시간 표시, 타이머, 스톱워치 중 하나 이상의 기능을 구현
+// 3. 랜덤 명언을 표시할 수 있는 컴포넌트 생성
+// 4. useState, useEffect, useRef 를 각각 한번 이상 사용
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+{
+  /* <Bonus>
+  json-server를 사용해 Todo 정보를 파일로 저장
+  Custom Hook 을 만들고 사용 */
 }
 
-export default App
+import './App.css';
+
+function HeadLine() {
+  return <h1>Todo List is now on your BukitList!</h1>;
+}
+
+function App() {
+  return (
+    <>
+      <header>
+        <HeadLine />
+      </header>
+      <nav>
+        <input />
+        <button>조회</button>
+      </nav>
+      <hr />
+      <main>
+        <h2>It's your Todo List!!</h2>
+        <span>번호</span>
+        <span>내용</span>
+        <span>기능</span>
+        <hr />
+        <ul>
+          <li>
+            아 존맛탱
+            <button>수정</button>
+            <button>삭제</button>
+          </li>
+        </ul>
+        <hr />
+        <input />
+        <button>추가</button>
+      </main>
+      <hr />
+      <footer>Create by. L</footer>
+    </>
+  );
+}
+
+export default App;
