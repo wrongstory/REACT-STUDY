d6627e2304893c63cb03ae20de6c740eb1fb171e
L <lmwoo0327@naver.com>
2025-05-29 14:59:47 +0900

    [React] @mixin 사용법

--- deep/Study-SCSS/src/App.jsx
+++ deep/Study-SCSS/src/App.jsx
@@ -6,26 +6,17 @@ function App() {
       <div className="test">테스트용 div 입니다</div>
       <h2>SCSS 변수 연습</h2>
       <ul>
-        <li>
-          안녕하세요
-          <p>p 요소 입니다</p>
-        </li>
-        <li>
-          반갑습니다
-          <section>Section 요소 입니다.</section>
-        </li>
-        <li>
-          SCSS를
-          <article>
-            Article 요소 입니다.
-            <h3>H3 요소입니다.</h3>
-          </article>
-        </li>
-        <li>
-          공부해봅시다
-          <h2>H2 요소입니다.</h2>
-        </li>
+        <li>안녕하세요</li>
+        <li>반갑습니다</li>
+        <li>SCSS를</li>
+        <li>공부해봅시다</li>
       </ul>
+      <section>
+        <article>SCSS문법에는</article>
+        <article>변수 사용과</article>
+        <article>중첩 문법과</article>
+        <article>믹스인 등이 있습니다.</article>
+      </section>
     </>
   );
 }
