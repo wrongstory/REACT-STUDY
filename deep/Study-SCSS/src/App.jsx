64a038b21ef19a858496c4ef324dacf30ce5d77f
L <lmwoo0327@naver.com>
2025-05-29 11:53:46 +0900

    [React] scss 변수 연습

--- deep/Study-SCSS/src/App.jsx
+++ deep/Study-SCSS/src/App.jsx
@@ -4,6 +4,13 @@ function App() {
   return (
     <>
       <div className="test">테스트용 div 입니다</div>
+      <h2>SCSS 변수 연습</h2>
+      <ul>
+        <li>안녕하세요</li>
+        <li>반갑습니다</li>
+        <li>SCSS를</li>
+        <li>공부해봅시다</li>
+      </ul>
     </>
   );
 }
