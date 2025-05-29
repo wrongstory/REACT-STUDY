ec2bff1b31ba6719497c6532c4fe0d8c48490f0b
L <lmwoo0327@naver.com>
2025-05-29 13:45:09 +0900

    [React] 중첩 문법 작성

--- deep/Study-SCSS/src/App.jsx
+++ deep/Study-SCSS/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
@@ -6,10 +6,25 @@ function App() {
       <div className="test">테스트용 div 입니다</div>
       <h2>SCSS 변수 연습</h2>
       <ul>
-        <li>안녕하세요</li>
-        <li>반갑습니다</li>
-        <li>SCSS를</li>
-        <li>공부해봅시다</li>
+        <li>
+          안녕하세요
+          <p>p 요소 입니다</p>
+        </li>
+        <li>
+          반갑습니다
+          <section>Section 요소 입니다.</section>
+        </li>
+        <li>
+          SCSS를
+          <article>
+            Article 요소 입니다.
+            <h3>H3 요소입니다.</h3>
+          </article>
+        </li>
+        <li>
+          공부해봅시다
+          <h2>H2 요소입니다.</h2>
+        </li>
       </ul>
     </>
   );
