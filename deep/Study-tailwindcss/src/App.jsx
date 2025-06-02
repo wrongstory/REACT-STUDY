d41e1080734c1e9bf5787c63b8eacdd3d76f0a37
L <lmwoo0327@naver.com>
2025-06-02 10:50:14 +0900

    [React]Tailwindcss 환경 설정 및 기초동작 확인

---
+++ deep/Study-tailwindcss/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-9xl text-blue-500">안녕하세요</div>
+      <div className="text-[150px] text-blue-500">안녕히가세요</div>
+    </div>
+  );
+}
+
+export default App;
