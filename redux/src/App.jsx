4308d85285c8314880d52e86f5164f800643912e
L <lmwoo0327@naver.com>
2025-06-09 11:12:12 +0900

    [React] reducer 단일 복합 사용법

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { combineReducers, legacy_createStore } from 'redux';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+
+const increment1 = {
+  type: 'increment1',
+};
+
+const decrement1 = {
+  type: 'decrement1',
+};
+
+const increment2 = {
+  type: 'increment2',
+};
+
+const decrement2 = {
+  type: 'decrement2',
+};
+
+const counter1Reducer = (state = 2, action) => {
+  switch (action.type) {
+    case 'increment1':
+      return state + 1;
+    case 'decrement1':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const counter2Reducer = (state = 4, action) => {
+  switch (action.type) {
+    case 'increment2':
+      return state + 1;
+    case 'decrement2':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({ counter1Reducer, counter2Reducer });
+
+export const store = legacy_createStore(rootReducer);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const counter1 = useSelector((state) => state.counter1Reducer);
+  const counter2 = useSelector((state) => state.counter2Reducer);
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Counter1 : {counter1}</div>
+      <button onClick={() => dispatch(increment1)}>+</button>
+      <button onClick={() => dispatch(decrement1)}>-</button>
+      <div>Counter2 : {counter2}</div>
+      <button onClick={() => dispatch(increment2)}>+</button>
+      <button onClick={() => dispatch(decrement2)}>-</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
