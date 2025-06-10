d39d342377f7607beff24ce0b1064ff4d70d5692
L <lmwoo0327@naver.com>
2025-06-10 10:38:03 +0900

    [React] useMemo 실습

--- optimization/src/App.jsx
+++ optimization/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState(0);
+  const [rerender, setRerender] = useState(false);
+
+  const plus1 = (number) => {
+    console.log("plus1 실행");
+    return number + 1;
+  };
+
+  const numberPlus1 = useMemo(() => {
+    return plus1(number);
+  }, [number]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>numberPlus1 : {numberPlus1}</div>
+      <button onClick={() => setNumber(number + 1)}>+1</button>
+      <button onClick={() => setRerender(!rerender)}>Rerender</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
